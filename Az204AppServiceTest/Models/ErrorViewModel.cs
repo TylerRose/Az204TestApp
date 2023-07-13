4f929a343f21bd214e4e0a8d8b920d9c8123125e
Tyler Rose <tylerrose25565@gmail.com>
2023-07-12 20:50:10 -0700

    Add project files.

---
+++ Az204AppServiceTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Az204AppServiceTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
